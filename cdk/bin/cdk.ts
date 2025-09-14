6d2a1407b37d4a9cecee16f3d2a2e11b451e1db9
Jesse <59843971+CaptainCode7@users.noreply.github.com>
2025-09-14 19:19:35 -0400

    ci: deploy pipeline wiring (SpaceweatherPipelineStack) and workflow owner/name fix [deploy-pipeline]

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 import { getConfig } from '../lib/config';
 
 const app = new cdk.App();
@@ -18,3 +19,19 @@ new CdkStack(app, 'CdkStack', {
     region: config.aws.region,
   },
 });
+
+// Optionally deploy the CDK Pipeline if requested via context
+const deployPipeline = app.node.tryGetContext('deploy-pipeline');
+if (deployPipeline && `${deployPipeline}`.toLowerCase() === 'true') {
+  const repoOwner = app.node.tryGetContext('repo-owner') || process.env.GITHUB_REPOSITORY?.split('/')[0] || 'CaptainCode7';
+  const repoName = app.node.tryGetContext('repo-name') || process.env.GITHUB_REPOSITORY?.split('/')[1] || 'spaceweather-news';
+
+  new PipelineStack(app, 'SpaceweatherPipelineStack', {
+    repoOwner,
+    repoName,
+    env: {
+      account: config.aws.account,
+      region: config.aws.region,
+    },
+  });
+}
